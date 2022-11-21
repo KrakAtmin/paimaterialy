55ff16cc843b706632fd41c075a35bf76882a5f9
MasnyKrak <kuperek121@wp.pl>
2022-11-21 18:40:07 +0100

    tabele 1/2 done

--- if i operatory (orange czy coś)/kalkulator/skrypt.js
+++ if i operatory (orange czy coś)/kalkulator/skrypt.js
@@ -2,41 +2,71 @@
 const calculate = () => {
     const firstNumber = Number(prompt("Podaj pierwszą liczbę"))
     const secondNumber = Number(prompt("Podaj drugą liczbę"))
-    const operator = String(prompt("Podaj operator działania"))
+    const operator = String(prompt("Podaj operator działania")) 
     const operatorArray = ['+', '-', '*', '/', '%', '**']
-    let result
-    if (String(firstNumber) != "NaN") {
-        if (String(secondNumber) != "NaN") {   
-            if (operatorArray.includes(operator)) {
-                if (operator == "+") {
-                    result = firstNumber + secondNumber
-                } else if (operator == "-") {
-                    result = firstNumber - secondNumber
-                } else if (operator == "*") {
-                    result = firstNumber * secondNumber
-                } else if (operator == "/") {
-                    if (secondNumber == 0) {
-                        alert("Nie dziel cholero przez 0")
-                    } else {
-                        result = firstNumber / secondNumber
-                    }
-                } else if (operator == "%") {
-                    if (firstNumber < secondNumber){
-                        alert("Druga liczba musi być mniejsza")
-                    } else {
-                        result = firstNumber % secondNumber
-                    }
-                } else if (operator == "**") {
-                    result = firstNumber ** secondNumber
-                }
-                alert(`Wynik: ${result}`)
-            } else {
-                alert("Dozwolone operatory: +, -, *, /, %, **")
-            }
-        } else {
-            alert("Podaj liczbę, a nie tekst")
-        }
-    } else {
-        alert("Podaj liczbę, a nie tekst")
+
+
+    const operations = {
+        '+': (a,b)=>a+b,
+        '-': (a,b)=>a-b,
+        '*': (a,b)=>a*b,
+        '/': (a,b)=>a/b,
+        '%': (a,b)=>a%b,
+        '**': (a,b)=>a**b,
     }
+
+    
+    const wynik = operations[operator](firstNumber, secondNumber);
+    const start = performance.now()
+    if (String(firstNumber) != "NaN" || String(secondNumber) != "NaN") {
+        if (operatorArray.includes(operator)) {
+            const end = performance.now()
+            console.log(`Performance: ${end - start} ms`)
+            alert(`Wynik to: ${wynik}`)
+        }
+    }
+
+
+
+
+
+
+
+    //    let result
+    //    if (String(firstNumber) != "NaN") {
+    //        if (String(secondNumber) != "NaN") {   
+    //            if (operatorArray.includes(operator)) {
+    //             if (operator == "+") {
+    //                 result = firstNumber + secondNumber
+    //             } else if (operator == "-") {
+    //                 result = firstNumber - secondNumber
+    //             } else if (operator == "*") {
+    //                 result = firstNumber * secondNumber
+    //             } else if (operator == "/") {
+    //                 if (secondNumber == 0) {
+    //                     alert("Nie dziel cholero przez 0")
+    //                 } else {
+    //                     result = firstNumber / secondNumber
+    //                 }
+    //             } else if (operator == "%") {
+    //                 if (firstNumber < secondNumber){
+    //                     alert("Druga liczba musi być mniejsza")
+    //                 } else {
+    //                     result = firstNumber % secondNumber
+    //                 }
+    //             } else if (operator == "**") {
+    //                 result = firstNumber ** secondNumber
+    //             }
+    //             const end = performance.now()
+    //             console.log(`Performance: ${end - start} ms`)
+    //               alert(`Wynik: ${result}`)
+    //          } else {
+    //              alert("Dozwolone operatory: +, -, *, /, %, **")
+    //          }
+    //      } else {
+    //          alert("Podaj liczbę, a nie tekst")
+    //      }
+    //  } else {
+    //      alert("Podaj liczbę, a nie tekst")
+    //  }
 }
---
+++ tablice-i-obiekty/skrypt.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    calculateZad1();
+    calculateZad2();
+    calculateZad3();
+});
+
+
+const calculateZad1 = () => {
+    const words = '18>2>2>14>1>18>3>2>6>6>15>11>16>13>9>10>1>17>15>14'
+    const stringArray = words.split('>')
+    console.log(stringArray)
+    console.log('Min:' + Math.min(...stringArray))
+    console.log('Max:' + Math.max(...stringArray))
+    const numberArray = stringArray.map(element => Number(element))
+    let sum = numberArray.reduce((a, b) => a + b, 0)
+    console.log('Suma:' + sum)
+    const average = sum / numberArray.length
+    console.log('Średnia:' + average)
+    console.log('Parzyste:' + numberArray.filter(element => element%2 === 0))
+    console.log('Nieparzyste:' + numberArray.filter(element => element%2 != 0))
+    console.log('Index 17-stki:' + numberArray.indexOf(17))
+    if (numberArray.includes(average)) {
+        console.log('Tablica zawiera średnią')
+    } else {
+        console.log('Tablica nie zawiera średniej')
+    }
+    function compareNumbers(a, b) {
+        return b - a;
+      }
+    console.log('Posortowana rosnąco tabela:' + numberArray.sort(compareNumbers))
+    let uniqueArray = []
+    // uniqueArray = numberArray.reduce(stage => {
+    //     if (uniqueArray.includes(stage)) {
+    //         return 0
+    //     }
+    //       uniqueArray.push(stage)
+ 
+    // })
+    console.log(uniqueArray)
+
+
+}
+
+const calculateZad2 = () => {
+
+}
+
+const calculateZad3 = () => {
+
+}
