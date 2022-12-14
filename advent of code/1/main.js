ac691f4ad6f6c98a48980c9d17ba7fd1861e7db2
MasnyKrak <kuperek121@wp.pl>
2022-12-14 19:15:31 +0100

    nie pamiętam co tam było, chyba test

---
+++ advent of code/1/main.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const list = `
+    1000
+    2000
+    3000
+    
+    4000
+    
+    5000
+    6000
+    
+    7000
+    8000
+    9000
+    
+    10000`
+
+    const array = list.split(" ")
+    array2 = Number(array)
+    let sum
+
+    // for (let i = 0; i < array2.length; i++) {
+    //   if (array2[i] == " ") {
+    //       break
+    //   }
+    // sum = sum + array2[i]
+      
+    // console.log(sum)
+    // }
+    
+    console.log(array)
+
+})
